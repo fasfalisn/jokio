a85903a7cea3278c482e7cb6e2898d530d03c3f8
Nikiforos Fasfalis <fasfalis.n@gmail.com>
2025-04-12 09:51:12 +0300

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Navbar from './components/navbar/Navbar.vue'
+import Footer from './components/footer/Footer.vue'
 </script>
 
 <template>
   <Navbar />
+  <Footer />
 </template>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="w-full bg-white text-teal-800 h-16 flex items-center justify-center p-5 shadow-md">
+    <p class="text-sm">© 2025 Jokio - Nikiforos Fasfalis</p>
+  </footer>
+</template>
