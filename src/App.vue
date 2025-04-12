164096fbdcaa63de20833e3317b6cc1624dfd7a3
Nikiforos Fasfalis <fasfalis.n@gmail.com>
2025-04-12 10:33:48 +0300

    feat: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Navbar from './components/navbar/Navbar.vue'
 import Footer from './components/footer/Footer.vue'
+import Header from './components/header/Header.vue'
 </script>
 
 <template>
   <Navbar />
+  <main class="container mx-auto">
+    <Header scrollTarget="#discovery" />
+  </main>
   <Footer />
 </template>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps(['scrollTarget'])
+console.log(props.scrollTarget)
+</script>
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-4xl font-bold text-teal-800">Welcome to Jokio</h1>
+    <p class="mt-4 text-lg text-gray-700">Your daily dose of jokes!</p>
+    <a
+      :href="scrollTarget"
+      class="mt-6 px-4 py-2 cursor-pointer bg-teal-800 text-white rounded hover:bg-teal-700"
+    >
+      Get Started
+    </a>
+  </div>
+</template>
